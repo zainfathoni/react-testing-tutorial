92d6e6516149c4e902d775cb2cb717f76cf99d34
Zain Fathoni <me@zainf.dev>
2023-07-04 22:05:01 +0800

    feat(test): Add code coverage configuration

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,6 +4,8 @@ export default defineConfig({
   test: {
     coverage: {
       all: true,
+      include: ["src/**/*.ts", "src/**/*.tsx"],
+      exclude: ["src/main.tsx", "src/vite-env.d.ts"],
     },
     environment: "happy-dom",
     globals: true,
